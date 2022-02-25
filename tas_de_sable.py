59dfc580efb6566405db686a49479282623423ae
Paul ROUILLIER <paul.rouillier@ens.uvsq.fr>
2022-02-25 18:52:45 +0100

    modif

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -4,3 +4,35 @@
 # Paul ROUILLIER
 # Hajar ZAAZOUA ggg
 # https://github.com/uvsq22106749/projet_tas_de_sable
+
+#CONFIGURATION#
+
+L = [[0] * 4 for i in range(4)]
+for i in range(4) :
+    for j in range(4) :
+        if i == j :
+            L[i][j] = 0
+        elif i < j :
+            L[i][j] = 0
+            
+for i in L:
+    print(' '.join([str(j) for j in i]))
+
+#INTERFACE GRAPHIQUE#
+
+import tkinter as tk
+
+HEIGHT = 400
+WIDTH = 500
+
+racine = tk.Tk() # Création de la fenêtre racine
+
+bouton1 = tk.Button(text="afficher config", font = ("Helvitica", "30"))
+bouton1.grid(column = 0, row = 1)
+
+canvas = tk.Canvas(racine, height = HEIGHT, width = WIDTH,)
+canvas.grid(column = 0, row = 0)
+
+racine.mainloop() # Lancement de la boucle principale
+
+#FONCTION#
